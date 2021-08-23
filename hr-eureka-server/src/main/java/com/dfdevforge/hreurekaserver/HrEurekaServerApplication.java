27d00cc98e7cbecf429790bd0627e78bb9ce3220
Walter Diego de Souza Freire <wdiegosfreire@yahoo.com.br>
2021-08-23 18:35:44 -0300

    Configuring Eureka Server

--- hr-eureka-server/src/main/java/com/dfdevforge/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/dfdevforge/hreurekaserver/HrEurekaServerApplication.java
@@ -2,12 +2,13 @@ package com.dfdevforge.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrEurekaServerApplication.class, args);
 	}
-
 }
