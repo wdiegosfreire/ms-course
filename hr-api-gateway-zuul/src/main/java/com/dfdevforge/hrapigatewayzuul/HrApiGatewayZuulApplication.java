8f09f03fddd67be7560a4ad08422afb131ed194c
Walter Diego de Souza Freire <wdiegosfreire@yahoo.com.br>
2021-08-23 21:40:42 -0300

    Configuring Zull api gateway

--- hr-api-gateway-zuul/src/main/java/com/dfdevforge/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/dfdevforge/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -2,12 +2,14 @@ package com.dfdevforge.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
 	}
-
 }
