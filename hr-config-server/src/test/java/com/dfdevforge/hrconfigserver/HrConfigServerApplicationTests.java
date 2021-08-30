c396fb2645fc1f6caf7f833e09bd580611ad0939
Walter Diego de Souza Freire <wdiegosfreire@yahoo.com.br>
2021-08-30 18:20:44 -0300

    Project hr-config-server created

---
+++ hr-config-server/src/test/java/com/dfdevforge/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfdevforge.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
