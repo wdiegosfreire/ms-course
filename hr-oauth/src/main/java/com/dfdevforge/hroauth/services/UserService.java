7416615703333f83b623a08ef518541346f377c5
Walter Diego de Souza Freire <wdiegosfreire@yahoo.com.br>
2021-09-04 11:39:48 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/dfdevforge/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/dfdevforge/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.dfdevforge.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.dfdevforge.hroauth.entities.User;
 import com.dfdevforge.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -22,7 +25,20 @@ public class UserService {
 			logger.error("Email not found: " + email);
 			throw new IllegalArgumentException("Email not found");
 		}
+
 		logger.info("Email found: " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if (user == null) {
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+
+		logger.info("Email found: " + username);
 		return user;
 	}
 }
